e00107702ae66cd6b8dc2aa4bbe6e6b8f0471647
Luke Stevens <luke.cs.stevens@gmail.com>
2017-04-13 18:30:27 -0500

    Adding animation and fixing incorrect logic

--- assets/js/front-end/controllers/actionSuccess.js
+++ assets/js/front-end/controllers/actionSuccess.js
@@ -12,18 +12,20 @@ define([], function() {
 					
 					success_message.html( response.data.actions.success_message ).show();
 					
-					//Let's check if the success message is already visible in the viewport without scrolling
+					//Let's check if the success message is already fully visible in the viewport without scrolling
 					var top_of_success_message = success_message.offset().top;
-				    	var bottom_of_success_message = success_message.offset().top + success_message.outerHeight();
-				    	var bottom_of_screen = jQuery(window).scrollTop() + jQuery(window).height();
+			    	var bottom_of_success_message = success_message.offset().top + success_message.outerHeight();
+			    	var bottom_of_screen = jQuery(window).scrollTop() + jQuery(window).height();
+			    	var top_of_screen = jQuery(window).scrollTop();
 
-				    	if(!((bottom_of_screen > top_of_success_message) && (bottom_of_screen < bottom_of_success_message))){
-						//The element isn't visible, so let's scroll to the success message as in the previous release
+			    	var the_element_is_visible = ((bottom_of_screen > bottom_of_success_message) && (top_of_screen < top_of_success_message));
+
+			    	if(!the_element_is_visible){
+						//The element isn't visible, so let's scroll to the success message as in the previous release, but with a short animation
 						jQuery('html, body').animate({
 							scrollTop: ( success_message.offset().top - 50 )
-						}, 0 );
+						}, 300 );
 				   	}	
-				
 				}
 			}
 		}
